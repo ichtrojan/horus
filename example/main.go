d9a558b0d52157351dae5139391051bf3e574bed
Michael Okoh <ordrizzy@gmail.com>
2021-02-26 20:22:35 +0100

    rewrite to return JSON

--- example/main.go
+++ example/main.go
@@ -1,32 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/ichtrojan/horus"
 	_ "github.com/ichtrojan/horus"
-	"io/ioutil"
-	"log"
 	"net/http"
 )
 
+type response struct {
+	Message string
+}
+
 func main() {
 	http.HandleFunc("/", horus.Watch(func(w http.ResponseWriter, r *http.Request) {
-		resp, err := http.Get("https://google.com/")
+		w.Header().Set("Content-Type", "application/json")
 
-		if err != nil {
-			fmt.Print(err)
-		}
-		body , err := ioutil.ReadAll(resp.Body)
-
-		defer resp.Body.Close()
-
-		w.WriteHeader(resp.StatusCode)
-		_, _ = fmt.Fprintf(w, string(body))
+		_ = json.NewEncoder(w).Encode(response{Message: "Horus is live 👁"})
 	}))
-
-	if err := horus.Serve(":8016"); err != nil{
-		log.Fatal(err)
-	}
 
 	if err := http.ListenAndServe(":8888", nil); err != nil {
 		fmt.Print(err)
