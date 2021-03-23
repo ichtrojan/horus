badfe1ab558a6f3f748eb5cbb312cf5f85873cef
Michael Okoh <ordrizzy@gmail.com>
2021-03-23 21:55:16 +0100

    undo login removal

--- models/request.go
+++ models/request.go
@@ -6,11 +6,11 @@ import (
 
 type Request struct {
 	gorm.Model
-	ResponseBody  string  `gorm:"type:varchar(10000);" json:"responsebody"`
+	ResponseBody  string  `gorm:"type:text;" json:"responsebody"`
 	ResposeStatus int     `gorm:"type:integer;" json:"code"`
-	RequestBody   string  `gorm:"type:varchar(10000);" json:"requestbody"`
-	Path          string  `gorm:"type:varchar(1000);" json:"path"`
-	Headers       string  `gorm:"type:varchar(10000);" json:"headers"`
+	RequestBody   string  `gorm:"type:text;" json:"requestbody"`
+	Path          string  `gorm:"type:text;" json:"path"`
+	Headers       string  `gorm:"type:text;" json:"headers"`
 	Method        string  `gorm:"type:varchar(10);" json:"method"`
 	Host          string  `gorm:"type:varchar(100);" json:"host"`
 	Ipadress      string  `gorm:"type:varchar(100);" json:"ipaddress"`
