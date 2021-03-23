e6a27a81e4aff02c744484a37ab31daad71d3dc4
Michael Okoh <ordrizzy@gmail.com>
2021-03-23 16:11:49 +0100

    cleanup

--- views/public/js/components/Logs.vue
+++ views/public/js/components/Logs.vue
@@ -1,33 +1,34 @@
 <template>
-    <tr>
-      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-        <p class="text-gray-900 whitespace-no-wrap">{{log.key}}</p>
-      </td>
-      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-        <p class="text-gray-900 whitespace-no-wrap">/</p>
-      </td>
-      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+  <tr>
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+      <p class="text-gray-900 whitespace-no-wrap">{{ log.key }}</p>
+    </td>
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+      <p class="text-gray-900 whitespace-no-wrap">/</p>
+    </td>
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
         <span
-          class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight"
+            class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight"
         >
           <span
-            aria-hidden
-            class="absolute inset-0 bg-green-200 opacity-50 rounded-full"
+              aria-hidden
+              class="absolute inset-0 bg-green-200 opacity-50 rounded-full"
           ></span>
           <span class="relative">200</span>
         </span>
-      </td>
-      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-        0.01ms
-      </td>
-      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-        21st Jan, 2021
-      </td>
-      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">View</td>
-    </tr>
+    </td>
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+      0.01ms
+    </td>
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+      21st Jan, 2021
+    </td>
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">View</td>
+  </tr>
 </template>
+
 <script>
 module.exports = {
-    props: ['log'],
+  props: ['log'],
 };
 </script>
--- views/public/js/components/Sortbar.vue
+++ views/public/js/components/Sortbar.vue
@@ -1,45 +1,45 @@
 <template>
-    <!-- This is inactive for now -->
-    <div class="my-2 flex sm:flex-row flex-col">
-        <div class="flex flex-row mb-1 sm:mb-0">
-            <div class="relative">
-                <select
-                        class="appearance-none h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-                    <option>Today</option>
-                    <option>7 days ago</option>
-                    <option>1 month ago</option>
-                </select>
-                <div
-                        class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                    </svg>
-                </div>
-            </div>
-            <div class="relative">
-                <select
-                        class="appearance-none h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
-                    <option>GET</option>
-                    <option>POST</option>
-                </select>
-                <div
-                        class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                    </svg>
-                </div>
-            </div>
+  <!-- This is inactive for now -->
+  <div class="my-2 flex sm:flex-row flex-col">
+    <div class="flex flex-row mb-1 sm:mb-0">
+      <div class="relative">
+        <select
+            class="appearance-none h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+          <option>Today</option>
+          <option>7 days ago</option>
+          <option>1 month ago</option>
+        </select>
+        <div
+            class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+          </svg>
         </div>
-        <div class="block relative">
+      </div>
+      <div class="relative">
+        <select
+            class="appearance-none h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
+          <option>GET</option>
+          <option>POST</option>
+        </select>
+        <div
+            class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+          </svg>
+        </div>
+      </div>
+    </div>
+    <div class="block relative">
             <span class="h-full absolute inset-y-0 left-0 flex items-center pl-2">
                 <svg viewBox="0 0 24 24" class="h-4 w-4 fill-current text-gray-500">
                     <path
-                            d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z">
+                        d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z">
                     </path>
                 </svg>
             </span>
-            <input placeholder="Search"
-                    class="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
-        </div>
+      <input placeholder="Search"
+             class="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"/>
     </div>
+  </div>
 </template>
